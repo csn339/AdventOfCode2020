193613c3fbddf2ebc101e25ddb9d7b75930ebf2c
Carina Negreanu <t-caneg@microsoft.com>
2020-12-21 16:33:33 +0000

    question 72

---
+++ AC7.py
@@ -0,0 +1,50 @@
+import re
+
+
+def score(dict_, key):
+    scored = 0
+    for key2 in dict_[key]:
+        if key2 == 'other':
+            scored += 0
+        else:
+            scored += int(dict_[key][key2])+int(dict_[key][key2])*score(dict_, key2)
+    return scored
+
+
+with open(r'AC\AC7.txt', 'r') as f:
+    list_rules = f.read().split('\n')
+
+dict_rules = dict(dict())
+
+for rule in list_rules:
+    key, vals = rule.split('contain')
+    key = key.replace(' bags', '')
+    key = key.strip()
+    dict_rules[key] = dict()
+    val_list = vals.split(',')
+    for elt in val_list:
+        elt = elt.strip()
+        nr = elt[0]
+        key_val = re.sub('bags|bag|\\.', '', elt[2:])
+        key_val = key_val.strip()
+        dict_rules[key][key_val] = nr
+
+col = 'shiny gold'
+matched = set([col])
+flag = True
+current_ct = 0
+while flag:
+    for key in dict_rules:
+        if len(matched.intersection(set(dict_rules[key]))) == 0:
+            continue
+        matched.add(key)
+    if current_ct != len(matched):
+        current_ct = len(matched)
+    else:
+        flag = False
+
+print(current_ct)
+print(score(dict_rules, col))
+
+
+
